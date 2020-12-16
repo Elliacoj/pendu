28a5261ff3914bf8072d1ffc0443e7ed0b9b303e
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-16 17:09:44 +0100

    Modif

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ let wordLettres = [];
 let wordLettresR = [];
 let wordPin = document.createElement("p");
 
-wordPin.style.width = "100%";
-wordPin.style.textAlign = "center";
+wordPin.id = "wordPin"
 game.append(wordPin);
 
 rules.innerHTML = "Quelques régles: " + "<br><br>" + "Vous pouvez choisir la difficulté pour votre nombre d'erreur autorisé (facile: 6, normal: 4, difficile: 2)." + "<br>";
@@ -27,12 +26,18 @@ game.append(tentative);
 input.id = "input";
 input.pattern ="[a-z]{1}"
 enter.id = "enter";
-enter.innerHTML = "Confirmer";
+enter.innerHTML = "Entrer";
 game.append(input);
 game.append(enter);
 
 function gameOn() {
+    let img = document.createElement("img");
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.src = "un.webp";
+    pendu.append(img);
     let points = 6;
+    let win = 0;
     tentative.innerHTML = "Nombre de tentavive: " + points;
     let num = random();
     let word = array[num];
@@ -40,34 +45,7 @@ function gameOn() {
         wordLettresR.push(word.substring(x, (x+1)));
     }
     wordChoice(num);
-
-    enter.addEventListener("click", function() {
-        let essay = input.value;
-        console.log(essay)
-        let length = 0;
-        letters.innerHTML += essay + "<br>";
-        for(let x = 0; x < word.length; x++) {
-            if(essay === wordLettresR[x].toLowerCase()) {
-                wordLettres.splice(x, 1, (essay + " "));
-            }
-            else {
-                length++
-            }
-        }
-        if(length === word.length) {
-            points--;
-        }
-        length = 0;
-        tentative.innerHTML = "Nombre de tentavive: " + points;
-        wordPin.innerHTML = "";
-        for(let y = 0; y < word.length; y++) {
-            wordPin.innerHTML += wordLettres[y];
-        }
-        if(points === 0) {
-            alert("Perdu");
-            reset();
-        }
-    });
+    choice(word, points, img, win);
 }
 
 function random() {
@@ -83,6 +61,7 @@ function wordChoice(random) {
 }
 
 function reset() {
+    pendu.removeChild(pendu.lastElementChild);
     wordLettres.splice(0, wordLettres.length);
     wordLettresR.splice(0, wordLettresR.length);
     wordPin.innerHTML = "";
@@ -90,5 +69,66 @@ function reset() {
     gameOn();
 }
 
+function choice(word, points, img, win) {
 
+    let test = function() {
+        if(input.value.length === 1) {
+            let essay = input.value;
+            let length = 0;
+            input.value = "";
+            letters.innerHTML += essay + "<br>";
+            for(let x = 0; x < word.length; x++) {
+                if(essay === wordLettresR[x].toLowerCase()) {
+                    wordLettres.splice(x, 1, (essay + " "));
+                    win++
+                }
+                else {
+                    length++
+                }
+            }
+            console.log(length);
+            if(length === word.length) {
+                points--;
+                image(points, img);
+            }
+            tentative.innerHTML = "Nombre de tentavive: " + points;
+            wordPin.innerHTML = "";
+            for(let y = 0; y < word.length; y++) {
+                wordPin.innerHTML += wordLettres[y];
+            }
+            console.log(win);
+            if(points === 0) {
+                alert("Perdu");
+                reset();
+                enter.removeEventListener("click", test);
+            }
+            else if(win === word.length) {
+                alert("Gagné");
+                reset();
+                enter.removeEventListener("click", test);
+            }
+        }
+    }
+    enter.addEventListener("click", test);
+}
 
+function image(points, img){
+    if(points === 5) {
+        img.src = "deux.webp";
+    }
+    else if(points === 4) {
+        img.src = "trois.webp";
+    }
+    else if(points === 3) {
+        img.src = "quatre.webp";
+    }
+    else if(points === 2) {
+        img.src = "cinq.webp";
+    }
+    else if(points === 1) {
+        img.src = "six.webp";
+    }
+    else if(points === 0) {
+        img.src = "sept.webp";
+    }
+}
